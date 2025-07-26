86add3e6ffb72b5800638658781f093cf2a883cb
Minh Nguyen Quang <nguyenquangminh22052000@gmail.com>
2025-07-26 08:36:49 +0700

    feat: setup eslint prettier (#2)
    * feat: setup eslint, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,68 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import js from "@eslint/js"
+import prettier from "eslint-config-prettier"
+import importPlugin from "eslint-plugin-import"
+import tseslint from "typescript-eslint"
 
-module.exports = defineConfig([
-  expoConfig,
+/** @type {import("eslint").Linter.FlatConfig[]} */
+const config = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    ignores: ['dist/*'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      // 🧠 Quy tắc nhóm và thứ tự import
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",      // React, NodeJS
+            "external",     // Thư viện ngoài
+            "internal",     // alias nội bộ như @/
+            ["parent", "sibling", "index"],
+            "object",
+            "type"
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      // ⚠️ Rule khác
+      "no-unused-vars": "off",
+      "import/no-unresolved": "off",
+    },
   },
-]);
+  {
+    rules: {
+      ...prettier.rules,
+    },
+  },
+]
+
+export default config
