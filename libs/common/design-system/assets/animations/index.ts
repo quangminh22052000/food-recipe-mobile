e236ceb29809016fb9e2f70e4ac338ef680e4c59
Nguyen Quang Minh <nguyenquangminh22052000@gmail.com>
2025-07-26 09:34:07 +0700

    feat: add assets

---
+++ libs/common/design-system/assets/animations/index.ts
@@ -0,0 +1,4 @@
+export const animations = {
+  confetti: require("./confetti-animation.json"),
+  liveCircle: require("./liveCircle.json"),
+}
---
+++ libs/common/design-system/assets/images/index.ts
@@ -0,0 +1,5 @@
+export const images = {
+  appLogo: require("./app-logo.png"),
+  enFlag: require("./en-flag.webp"),
+  viFlag: require("./vi-flag.webp"),
+}
