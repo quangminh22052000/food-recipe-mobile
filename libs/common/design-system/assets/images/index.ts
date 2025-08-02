fe060d41926c62b0b307a54ebd8d1cf71a1a801a
Nguyen Quang Minh <nguyenquangminh22052000@gmail.com>
2025-08-02 11:59:52 +0700

    feat: add new recipes

--- libs/common/design-system/assets/images/index.ts
+++ libs/common/design-system/assets/images/index.ts
@@ -1,9 +1,13 @@
 export const images = {
   appLogo: require("./app-logo.png"),
   bunBo: require("./bun-bo.webp"),
+  comChienDuongChau: require("./com-chien-duong-chau.webp"),
+  comGa: require("./com-ga.webp"),
   comSuon: require("./com-suon.webp"),
+  comTronHanQuoc: require("./com-tron-han-quoc.webp"),
   enFlag: require("./en-flag.webp"),
   huTieu: require("./hu-tieu.webp"),
+  miQuang: require("./mi-quang.webp"),
   noodle: require("./noodle.webp"),
   phoBo: require("./pho-bo.webp"),
   rice: require("./rice.webp"),
