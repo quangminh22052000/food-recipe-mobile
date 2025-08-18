dbdcb975cc0704ae5ca2ee27563709b45c179f70
Nguyen Quang Minh <nguyenquangminh22052000@gmail.com>
2025-08-18 10:46:57 +0700

    feat: integrate mmkv

---
+++ libs/common/store/mmkvStorage.ts
@@ -0,0 +1,12 @@
+import { MMKV } from "react-native-mmkv"
+
+export const storage = new MMKV()
+
+// // Lưu dữ liệu
+// storage.set('token', 'abc123');
+
+// // Lấy dữ liệu
+// const token = storage.getString('token');
+
+// // Xóa dữ liệu
+// storage.delete('token');
